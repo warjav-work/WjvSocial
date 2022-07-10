36ca8097e29997fd1de854c80402ce1d980b4147
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2022-07-10 12:10:33 +0200

    Agregar archivos de proyecto.

---
+++ WjvSocial.Api/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WjvSocial.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(new {Id= 1, Text="Hola mundo!"});
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+
+            return Ok(new { Id = id, Text = "Hola mundo!" });
+        }
+    }
+}
